c4e76f47987669b21f315d2426e5d4487c425e0f
Alex Ahern <alex.ahern@bbc.co.uk>
2019-04-05 17:29:21 +0100

    Adding the view to the CrimeListFragment

--- app/src/main/java/com/bignerdranch/android/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/CrimeListFragment.kt
@@ -1,6 +1,26 @@
 package com.bignerdranch.android
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 class CrimeListFragment : Fragment() {
+    lateinit var crimeRecyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_crime_list,
+            container, false)
+        crimeRecyclerView = v.findViewById(R.id.crime_recycler_view) as RecyclerView
+        crimeRecyclerView.layoutManager = LinearLayoutManager(activity)
+
+        return v
+    }
 }
