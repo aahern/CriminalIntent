b41eec80387ac80f02b871ebbbc6ae398e842845
Alex Ahern <alex.ahern@bbc.co.uk>
2019-04-05 17:47:06 +0100

    Adding the recyclerview for CrimeList

--- app/src/main/java/com/bignerdranch/android/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/CrimeListFragment.kt
@@ -7,20 +7,57 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.list_item_crime.view.*
 
 class CrimeListFragment : Fragment() {
-    lateinit var crimeRecyclerView: RecyclerView
+    private lateinit var crimeRecyclerView: RecyclerView
+    private lateinit var crimeAdapter: CrimeAdapter
+
+    private class CrimeHolder(
+        inflater: LayoutInflater,
+        parent: ViewGroup
+    ) : RecyclerView.ViewHolder(
+        inflater.inflate(R.layout.list_item_crime, parent, false)
+    )
+
+    private class CrimeAdapter(val crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            return CrimeHolder(layoutInflater, parent)
+        }
+
+        override fun getItemCount(): Int {
+            return crimes.size
+        }
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            holder.itemView.crime_title.text = crimes[position].title
+            holder.itemView.crime_date.text = crimes[position].date.toString()
+        }
+
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v = inflater.inflate(R.layout.fragment_crime_list,
-            container, false)
+        val v = inflater.inflate(
+            R.layout.fragment_crime_list,
+            container, false
+        )
         crimeRecyclerView = v.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(activity)
 
+        updateUI()
         return v
+    }
+
+    private fun updateUI() {
+        val crimes = CrimeLab.crimes
+
+        crimeAdapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = crimeAdapter
     }
 }
