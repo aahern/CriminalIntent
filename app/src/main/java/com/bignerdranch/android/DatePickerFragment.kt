6265399b1e52f71dbd6b0f06d1a714ce3939b08a
Alex Ahern <alex.ahern@bbc.co.uk>
2019-04-07 12:21:28 +0100

    Adding the Calendar view within the AlertDialog

--- app/src/main/java/com/bignerdranch/android/DatePickerFragment.kt
+++ app/src/main/java/com/bignerdranch/android/DatePickerFragment.kt
@@ -4,10 +4,15 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
 
 class DatePickerFragment: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val v = LayoutInflater.from(activity)
+            .inflate(R.layout.dialog_date, null)
+
         return AlertDialog.Builder(activity)
+            .setView(v)
             .setTitle(R.string.date_picker_title)
             .setPositiveButton(android.R.string.ok, null)
             .create()
