eb181d7db1c3c7bdad4e52f085f801dd0283db51
Alex Ahern <alex.ahern@bbc.co.uk>
2019-04-11 11:33:16 +0100

    Adding a menu to the CrimeListFragment with add item icon

--- app/src/main/java/com/bignerdranch/android/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/CrimeListFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -78,6 +76,16 @@ class CrimeListFragment : Fragment() {
 
         updateUI()
         return v
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.fragment_crime_list, menu)
     }
 
     private fun updateUI() {
